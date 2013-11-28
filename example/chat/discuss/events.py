dbaf743e8e38667f646f31073bacd84b24131177
Geoffrey Lehée <hello@socketubs.org>
2013-11-28 21:19:18 +0100

    Added vulgare channel

--- example/chat/discuss/events.py
+++ example/chat/discuss/events.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+from bagou.utils import broadcast
 from bagou.events import on_close
 from bagou.events import on_message
 from bagou.events import on_subscribe
-from bagou.utils import broadcast
 
 
 @on_message(channel=r".*")
@@ -22,24 +22,22 @@ def broadcast_new_user(client, channel, message, callback):
         callback=callback,
         data={
             'name': 'System',
-            'text': '%s user join chat room.' % client.store.get('username', 'Unknown')})
+            'text': '%s join chat room.' % client.store.get('username', 'Unknown')})
 
 
 @on_close
-def broadcast_left_user(client, channel, message, callback):
+def broadcast_left_user(client):
     broadcast(
         event='message',
         channel=client.channels,
-        callback=callback,
         data={
             'name': 'System',
-            'text': '%s user left chat room.' % client.store.get('username', 'Unknown')})
+            'text': '%s left chat room.' % client.store.get('username', 'Unknown')})
 
 
-@on_subscribe(channel=r".*[dick|boobs|ass].*")
+@on_subscribe(channel=r"dick|boobs|ass")
 def vulgare_channel(client, channel, message, callback):
     client.jsonify(
         event='message',
-        channel="dick", data={
-            'name': 'BotMaster',
-            'text': 'You chat room is dirty, if u know what i mean.'})
+        channel="dick",
+        data={'name': 'System', 'text': 'You chat room is dirty, if u know what i mean.'})
